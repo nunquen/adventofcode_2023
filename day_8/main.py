8adffc2eae4057c1f94d05b31273b75c88b6671c
Saul Maldonado <saul.maldonado@arup.com>
2023-12-11 17:31:41 +0100

    day8 part1 still not working

--- day_8/main.py
+++ day_8/main.py
@@ -2,6 +2,7 @@ from lib.utils import (
     get_local_data_as_list,
     get_nodes_from_data,
     LocalFile,
+    Direction,
 )
 
 '''
@@ -11,6 +12,7 @@ Reference: https://adventofcode.com/2023/day/8
 
 
 def test():
+    directions = "RL"
     input_data = [
         "AAA = (BBB, CCC)",
         "BBB = (DDD, EEE)",
@@ -20,9 +22,28 @@ def test():
         "GGG = (GGG, GGG)",
         "ZZZ = (ZZZ, ZZZ)",
     ]
-    data = get_nodes_from_data(input_data=input_data)
+    nodes = get_nodes_from_data(input_data=input_data)
 
-    assert len(data) == 0
+    direction = Direction(
+        directions=directions,
+        nodes=nodes
+        )
+
+    steps = direction.get_steps()
+    assert steps == 2
+
+    input_data = [
+        "AAA = (BBB, BBB)",
+        "BBB = (AAA, ZZZ)",
+        "ZZZ = (ZZZ, ZZZ)",
+    ]
+    nodes = get_nodes_from_data(input_data=input_data)
+    direction = Direction(
+        directions="LLR",
+        nodes=nodes
+        )
+    steps = direction.get_steps()
+    assert steps == 6
     return True
 
 
@@ -33,8 +54,17 @@ if __name__ == "__main__":
 
     input_file = LocalFile.day_input_file.value
     input_data = get_local_data_as_list(input_file=input_file)
+    directions = input_data[0]
+    input_data = input_data[2:]
+    nodes = get_nodes_from_data(input_data=input_data)
 
-    print("Day 8. Spets to reach destination. Part1. Value: {} ".format(
-        None
+    direction = Direction(
+        directions=directions,
+        nodes=nodes
+        )
+
+    steps = direction.get_steps()
+    print("Day 8. Steps to reach destination. Part1. Value: {} ".format(
+        steps
         )
     )
