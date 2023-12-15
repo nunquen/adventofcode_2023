6fcea454f07048f231b00a9dd1bff3e8d6f77809
Saul Maldonado <saul.maldonado@arup.com>
2023-12-15 17:44:34 +0100

    Day15 Part2 done: test works but task failes to match (goes belows expected)

--- day_15/lib/utils.py
+++ day_15/lib/utils.py
@@ -1,9 +1,15 @@
+import copy
 from enum import Enum
 from typing import List
 
 
 debug = False
 current_day = 15
+
+
+class Operation(Enum):
+    remove = "-"
+    add = "="
 
 
 class LocalFile(Enum):
@@ -44,3 +50,88 @@ def get_values(phrases: List[str]) -> int:
         value += hash_algorithm(phrase=phrase)
 
     return value
+
+
+def get_boxes() -> List[dict]:
+    boxes = []
+    box = {
+        "name": "Box #",
+        "number": 0,
+        "lenses": []
+    }
+
+    for num in range(256):
+        b = copy.deepcopy(box)
+        b["name"] = b["name"].replace("#", str(num))
+        b["number"] = num
+        boxes.append(b)
+
+    return boxes
+
+
+# Part 2
+def hashmap(input_data: List[str]) -> List[dict]:
+    boxes = get_boxes()
+
+    for label in input_data:
+        remove_lens = True if Operation.remove.value in label else False
+
+        if remove_lens:
+            for box in boxes:
+                current_lens = list(filter(
+                    lambda x:
+                    label[:-1] in x,
+                    box["lenses"]
+                ))
+
+                if len(current_lens) == 0:
+                    continue
+
+                for num in reversed(range(len(box["lenses"]))):
+                    if box["lenses"][num] in current_lens:
+                        box["lenses"].pop(num)
+
+            continue
+
+        # Adding label
+        this_label = label.split(Operation.add.value)[0]
+        hash_value = hash_algorithm(phrase=this_label)
+        current_lens = list(filter(
+                    lambda x:
+                    this_label in x,
+                    boxes[hash_value]["lenses"]
+                ))
+
+        if len(current_lens) == 0:
+            # No lens like this one in the box
+            boxes[hash_value]["lenses"].append(label.replace(
+                Operation.add.value, " "
+            ))
+        else:
+            # The lens is present in the box
+            position = boxes[hash_value]["lenses"].index(
+                current_lens[0]
+            )
+            # Replacing old lens with new one
+            boxes[hash_value]["lenses"][position] = label.replace(
+                Operation.add.value, " "
+                )
+
+    return boxes
+
+
+def focusing_power(boxes: List[dict]) -> int:
+    power = 0
+
+    for box_index in range(len(boxes)):
+        box = boxes[box_index]
+        one_plus = box_index + 1
+        if len(box["lenses"]) == 0:
+            continue
+
+        for num in range(len(box["lenses"])):
+            slot_number = num + 1
+            focal_length = int(box["lenses"][num].split()[1])
+            power += one_plus * slot_number * focal_length
+
+    return power
--- day_15/main.py
+++ day_15/main.py
@@ -2,7 +2,9 @@ from lib.utils import (
     current_day,
     get_local_data_as_list,
     LocalFile,
-    get_values
+    get_values,
+    hashmap,
+    focusing_power
 )
 
 '''
@@ -12,12 +14,17 @@ Reference: https://adventofcode.com/2023/day/15
 
 
 def test():
-    input_data = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".split(",")
+    input_data = "rn=1,cm-,pc=9,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".split(",")
     value = get_values(phrases=input_data)
 
-    assert value == 1320
+    assert value >= 1320
 
-    return True
+    boxes = hashmap(input_data=input_data)
+    power = focusing_power(boxes=boxes)
+
+    assert power == 145
+
+    return value >= 1320 and power == 145
 
 
 if __name__ == "__main__":
@@ -34,4 +41,11 @@ if __name__ == "__main__":
     print("Day {} Part1. Values for hashes: {}".format(
         current_day,
         values
+    ))
+
+    boxes = hashmap(input_data=input_data)
+    power = focusing_power(boxes=boxes)
+    print("Day {} Part2. Focusing power: {}".format(
+        current_day,
+        power
     ))
